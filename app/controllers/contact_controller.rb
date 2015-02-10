a59e527e32293ce40ffbd462ef2404665b6f1bad
Matthew Cullum <creativelyconcise@gmail.com>
2015-02-10 12:56:13 -0700

    mail config

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -4,7 +4,7 @@ class ContactController < ApplicationController
 	end
 
 	def send_message
-		 ContactMailer.contact_email(params.inspect).deliver_later
+		 ContactMailer.contact_email(params.inspect).deliver
 		 redirect_to 'contact'
 	end
 end
